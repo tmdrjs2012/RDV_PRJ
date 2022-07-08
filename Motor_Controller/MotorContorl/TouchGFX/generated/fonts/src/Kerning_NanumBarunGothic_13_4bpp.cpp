504c6d28915f5053c7e0b97299e11edff0815ebb
sgson <tmdrjs2012@naver.com>
2022-07-08 17:09:56 +0900

    RDV MotorController TouchGFX GUI and Files

---
+++ Motor_Controller/MotorContorl/TouchGFX/generated/fonts/src/Kerning_NanumBarunGothic_13_4bpp.cpp
@@ -0,0 +1,15 @@
+#include <touchgfx/Font.hpp>
+
+FONT_KERNING_LOCATION_FLASH_PRAGMA
+KEEP extern const touchgfx::KerningNode kerning_NanumBarunGothic_13_4bpp[] FONT_KERNING_LOCATION_FLASH_ATTRIBUTE = {
+    { 0x0059, -1 },  // (First char = [0x0059, cid58], Second char = [0x0041, cid34], Kerning dist = -1)
+    { 0x004C, -1 },  // (First char = [0x004C, cid45], Second char = [0x0054, cid53], Kerning dist = -1)
+    { 0x004C, -1 },  // (First char = [0x004C, cid45], Second char = [0x0056, cid55], Kerning dist = -1)
+    { 0x004C, -1 },  // (First char = [0x004C, cid45], Second char = [0x0059, cid58], Kerning dist = -1)
+    { 0x0054, -1 },  // (First char = [0x0054, cid53], Second char = [0x0061, cid66], Kerning dist = -1)
+    { 0x0054, -1 },  // (First char = [0x0054, cid53], Second char = [0x0063, cid68], Kerning dist = -1)
+    { 0x0054, -1 },  // (First char = [0x0054, cid53], Second char = [0x0065, cid70], Kerning dist = -1)
+    { 0x0054, -1 },  // (First char = [0x0054, cid53], Second char = [0x006F, cid80], Kerning dist = -1)
+    { 0x0054, -1 },  // (First char = [0x0054, cid53], Second char = [0x0072, cid83], Kerning dist = -1)
+    { 0x0054, -1 },  // (First char = [0x0054, cid53], Second char = [0x0079, cid90], Kerning dist = -1)
+};
---
+++ Motor_Controller/MotorContorl/TouchGFX/gui/include/gui/mainscreen_screen/MainScreenView.hpp
@@ -0,0 +1,21 @@
+#ifndef MAINSCREENVIEW_HPP
+#define MAINSCREENVIEW_HPP
+
+#include <gui_generated/mainscreen_screen/MainScreenViewBase.hpp>
+#include <gui/mainscreen_screen/MainScreenPresenter.hpp>
+
+class MainScreenView : public MainScreenViewBase
+{
+public:
+    MainScreenView();
+    virtual ~MainScreenView() {}
+    virtual void setupScreen();
+    virtual void tearDownScreen();
+
+    virtual void handleTickEvent();
+protected:
+    virtual void toggleButtonClcked();
+    virtual void SlideChanged(int value);
+};
+
+#endif // MAINSCREENVIEW_HPP
